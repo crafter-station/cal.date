b2f03a40b945761e7918ada07c34b3ee3fb1e05e
Cris <cristian.correa.cs@gmail.com>
2026-01-11 23:38:30 -0500

    Update URLs to use @username format

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,30 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**.public.blob.vercel-storage.com",
+      },
+      {
+        protocol: "https",
+        hostname: "ssf0txms9kpjxnoo.public.blob.vercel-storage.com",
+      },
+    ],
+  },
+  async rewrites() {
+    return [
+      {
+        source: "/@:username",
+        destination: "/:username",
+      },
+      {
+        source: "/@:username/og",
+        destination: "/:username/og",
+      },
+    ];
+  },
 };
 
 export default nextConfig;
